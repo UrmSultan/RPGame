f41440eea78a3d20440a737b6616e66f6fc95bb5
Urmanbetov Sultan <urmanbetov.sultan@gmail.com>
2025-02-04 17:41:04 +0600

    added class Location

--- include/Location.h
+++ include/Location.h
@@ -1,7 +1,27 @@
 #pragma once
+#include <bits/stdc++.h>
+#include "Enemy.h"
+#include "Item.h"
 
+using namespace std;
 
 class Location {
+private:
+    string name;
+    string description;
+    vector <shared_ptr<Location>> connectedLocations;
+    vector <shared_ptr<Enemy>> enemies;
+    vector <shared_ptr<Item>> items;
+public:
+    Location(string n, string desc);
+    void addConnection(const shared_ptr<Location>& location);
+    void addEnemy(const shared_ptr<Enemy>& enemy);
+    void addItem(const shared_ptr<Item>& item);
+    void describe() const;
+    [[nodiscard]] vector <shared_ptr<Location>> getConnections() const;
+    [[nodiscard]] vector <shared_ptr<Enemy>> getEnemies() const;
+    [[nodiscard]] vector <shared_ptr<Item>> getItems() const;
+    [[nodiscard]] string getName() const;
 
 };
 
--- src/Location.cpp
+++ src/Location.cpp
@@ -1,5 +1,43 @@
-//
-// Created by sulta on 03.02.2025.
-//
-
+#include <bits/stdc++.h>
 #include "../include/Location.h"
+
+Location::Location(string n, string desc)
+    :name(std::move(n)), description(std::move(desc))   {}
+
+void Location::addConnection(const shared_ptr<Location>& location) {
+    connectedLocations.push_back(location);
+}
+
+void Location::addEnemy(const shared_ptr<Enemy> &enemy) {
+    enemies.push_back(enemy);
+}
+
+void Location::addItem(const shared_ptr<Item> &item) {
+    items.push_back(item);
+}
+
+void Location::describe() const {
+    cout<<"Вы находитесь в "<<name<<". "<<endl;
+    if (!connectedLocations.empty()) {
+        cout<<"Вы можете пройти в:"<<endl;
+        for (const auto& loc: connectedLocations) {
+            cout<<"- "<<loc->name<<endl;
+        }
+    }
+}
+
+vector<shared_ptr<Location>> Location::getConnections() const {
+    return connectedLocations;
+}
+
+string Location::getName() const {
+    return name;
+}
+
+vector<shared_ptr<Enemy>> Location::getEnemies() const {
+    return enemies;
+}
+
+vector<shared_ptr<Item>> Location::getItems() const {
+    return items;
+}
